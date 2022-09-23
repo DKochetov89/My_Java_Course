9cc5a80f5835b23e4ac13d7f0133f5f4dfa9b741
DKochetov89 <109920237+DKochetov89@users.noreply.github.com>
2022-09-23 15:35:53 +1100

    Create OrderProcessor_Pre_Post.java

---
+++ 1.OOP/src/solid/open_closed/OrderProcessor_Pre_Post.java
@@ -0,0 +1,14 @@
+package solid.open_closed;
+
+import solid.single_responsibility.OrderProcessor;
+
+public class OrderProcessor_Pre_Post extends OrderProcessor {
+
+    void preOrderProcess() {
+        //pre order processing
+    }
+
+    void postOrderProcess() {
+        //post order processing
+    }
+}
