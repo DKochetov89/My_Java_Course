6950bca10596665dc55cb676f842ccb18323ce15
DKochetov89 <109920237+DKochetov89@users.noreply.github.com>
2022-09-23 15:34:55 +1100

    Create OrderValidator_v1.java

---
+++ 1.OOP/src/solid/liskov/OrderValidator_v1.java
@@ -0,0 +1,9 @@
+package solid.liskov;
+
+import solid.single_responsibility.OrderProcessor;
+
+public class OrderValidator_v1 {
+    public void isValid(OrderProcessor orderProcessor) {
+        // order validation
+    }
+}
