515469bdf8d5eb84589249dfd2b27b8368952a15
Dmitriy Kochetov <109920237+DKochetov89@users.noreply.github.com>
2022-09-27 16:43:04 +1100

    Create Anonymous_Test.java

---
+++ 2.Java_Core_Part_2/inner_classes/Anonymous_Test.java
@@ -0,0 +1,29 @@
+package inner_classes;
+
+public class Anonymous_Test {
+
+    void method() {
+        System.out.println("method экземпляра класса Anonymous_Test");
+    }
+
+    public static void main(String[] args) {
+        Anonymous_Test anonymous_test = new Anonymous_Test() {
+            {
+                super.method();
+                method();
+                new_method();
+                }
+
+                @Override
+                void method() { // переопределение метода в анонимном классе
+                    System.out.println("method экземпляра анонимного класса ");
+                }
+
+                void new_method() {
+                    //новый метод который мы добавили
+                    System.out.println("new_method экземпляра анонимного класса ");
+                }
+            };
+        anonymous_test.method();
+    }
+}
