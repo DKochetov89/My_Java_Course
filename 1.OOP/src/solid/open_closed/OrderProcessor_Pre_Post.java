3e9dd48a19f2f43c79ab38b8ccfbff38c9f1541b
Dmitriy Kochetov <109920237+DKochetov89@users.noreply.github.com>
2022-10-30 07:27:22 +1100

    Update OrderProcessor_Pre_Post.java

--- 1.OOP/src/solid/open_closed/OrderProcessor_Pre_Post.java
+++ 1.OOP/src/solid/open_closed/OrderProcessor_Pre_Post.java
@@ -1,3 +1,9 @@
+/*
+O: Open-Closed Principle (Принцип открытости-закрытости).
+Программные сущности (классы, модули, функции) должны быть открыты для расширения, но не для модификации.
+class OrderProcessor_Pre_Post extends OrderProcessor - расширяет OrderProcessor
+*/
+
 package solid.open_closed;
 
 import solid.single_responsibility.OrderProcessor;
