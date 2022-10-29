3d0c1b0219fa968a30063de86966dbf4db1ef845
Dmitriy Kochetov <109920237+DKochetov89@users.noreply.github.com>
2022-10-30 07:16:23 +1100

    Update Server.java

--- 1.OOP/src/association/Server.java
+++ 1.OOP/src/association/Server.java
@@ -1,7 +1,12 @@
 package association;
 
 import java.util.List;
-//Aggregation
+
+/*
+Агрегация предполагает, что объекты связаны взаимоотношением «part-of» (часть). 
+Агрегация — частный случаи ассоциации «часть-целое».
+*/
+
 public class Server {
 
     List<ModuleRAM> serverRAM;
