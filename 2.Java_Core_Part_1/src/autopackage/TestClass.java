6818199af38b680bf36a2fdfe83df0ef686e50b3
Dmitriy Kochetov <d.kochetov89@yandex.ru>
2023-03-23 05:48:40 +1100

    is deprecated and marked for removal

--- 2.Java_Core_Part_1/src/autopackage/TestClass.java
+++ 2.Java_Core_Part_1/src/autopackage/TestClass.java
@@ -15,33 +15,43 @@ public class TestClass {
         char _char = '7';
         boolean _boolean = true;
 
+        //deprecated
         Byte $byte = new Byte(_byte);
         System.out.println($byte.getClass());
 
+        //deprecated
         Short $short = new Short(_short);
         System.out.println($short.getClass());
 
+        //deprecated
         Integer $integer = new Integer(_int);
         System.out.println($integer.getClass());
 
+        //deprecated
         BigInteger bigInteger = new BigInteger(String.valueOf(_int));
         System.out.println(bigInteger.getClass());
 
+        //deprecated
         Long $long = new Long(_long);
         System.out.println($long.getClass());
 
+        //deprecated
         Float $float = new Float(_float);
         System.out.println($float.getClass());
 
+        //deprecated
         Double $double = new Double(_double);
         System.out.println($double.getClass());
 
+        //deprecated
         BigDecimal bigDecimal = new BigDecimal(_double);
         System.out.println(bigDecimal.getClass());
 
+        //deprecated
         Character $character = new Character(_char);
         System.out.println($character.getClass());
 
+        //deprecated
         Boolean $boolean = new Boolean(_boolean);
         System.out.println($boolean.getClass());
 
